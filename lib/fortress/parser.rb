5c52644722d45f01833b7ce158d84ab1a9be2ddd
Cássio Marques <cassio.marques@baby.com.br>
2013-08-05 13:46:09 -0300

    Parser for siege output

--- lib/fortress/parser.rb
+++ lib/fortress/parser.rb
@@ -1,6 +1,21 @@
 # encoding: utf-8
 module Fortress
   class Parser
+    attr_reader :output, :transactions, :availability, :response_time, :transaction_rate,
+      :throughput, :concurrency, :failed_transactions, :longest_transaction, :shortest_transaction
 
+    def initialize(output)
+      @output               = output
+      @transactions         = /Transactions:\s+(\d+)\s+hits/.match(output)[1].to_i
+      @availability         = /Availability:\s+([\d\.]+)\s%/.match(output)[1].to_f
+      @response_time        = /Response time:\s+([\d\.]+)\s/.match(output)[1].to_f
+      @transaction_rate     = /Transaction rate:\s+([\d\.]+)\s/.match(output)[1].to_f
+      throughput_matches    = /Throughput:\s+([\d\.]+)\s(.+)$/.match(output)
+      @throughput           = {:value => throughput_matches[1].to_f, :unit => throughput_matches[2]}
+      @concurrency          = /Concurrency:\s+([\d\.]+)$/.match(output)[1].to_f
+      @failed_transactions  = /Failed transactions:\s+([\d\.]+)/.match(output)[1].to_i
+      @longest_transaction  = /Longest transaction:\s+([\d\.]+)/.match(output)[1].to_f
+      @shortest_transaction = /Shortest transaction:\s+([\d\.]+)/.match(output)[1].to_f
+    end
   end
 end
--- spec/fortress/parser_spec.rb
+++ spec/fortress/parser_spec.rb
@@ -2,7 +2,59 @@
 require 'spec_helper'
 
 describe Fortress::Parser do
-  it "works" do
-    1.should == 1
+  let(:parser) { described_class.new(output) }
+  let(:output) {
+    <<OUTPUT
+Lifting the server siege...      done.
+
+Transactions:                     12 hits
+Availability:                 100.00 %
+  Elapsed time:                   9.97 secs
+Data transferred:               2.22 MB
+Response time:                  1.73 secs
+Transaction rate:               1.20 trans/sec
+Throughput:                     0.22 MB/sec
+Concurrency:                    2.08
+Successful transactions:          12
+Failed transactions:               0
+Longest transaction:            2.26
+Shortest transaction:           0.88
+OUTPUT
+  }
+
+  it "parses the number of transactions" do
+    parser.transactions.should == 12
+  end
+
+  it "parses the availability" do
+    parser.availability.should == 100.00
+  end
+
+  it "parses the response time" do
+    parser.response_time.should == 1.73
+  end
+
+  it "parses the transaction rate" do
+    parser.transaction_rate.should == 1.20
+  end
+
+  it "parses the throughput" do
+    parser.throughput.should == {:value => 0.22, :unit => "MB/sec"}
+  end
+
+  it "parses the concurrency" do
+    parser.concurrency.should == 2.08
+  end
+
+  it "parses the number of failed transactions" do
+    parser.failed_transactions.should == 0
+  end
+
+  it "parses the time of the longest transaction" do
+    parser.longest_transaction.should == 2.26
+  end
+
+  it "parses the time of the shortest transaction" do
+    parser.shortest_transaction.should == 0.88
   end
 end
